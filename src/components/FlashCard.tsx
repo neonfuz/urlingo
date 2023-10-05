98e6221c8b2bf57f84e8a8f0186dfb0301d45032
Sage Raflik <neonfuz@gmail.com>
2023-10-05 01:18:30 -0500

    Make build succeed

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -9,15 +9,17 @@ type FlashCardData = {
   onNext: () => void;
 };
 export const FlashCard = (props: FlashCardData) => {
-  return (
-  <div class="bg-white p-4 border-2 w-[30ch] rounded-lg text-center">
-    <div class="text-slate-500 text-left">deck: {props.card.deck}</div>
-    {templates[props.card.template].front(props.card as any)}
-    <hr class="h-px my-8 bg-slate-300 border-0"></hr>
-    {templates[props.card.template].back(props.card as any, props.showBack)}
-    {!props.showBack
-      ? <Button onClick={props.onReveal}>Reveal</Button>
-      : <Button onClick={props.onNext}>Next</Button>}
-  </div>
+  // Check if props.card is defined to make build succeed
+  // TODO: investigate why this may be undefined?
+  return props.card && (
+    <div class="bg-white p-4 border-2 w-[30ch] rounded-lg text-center">
+      <div class="text-slate-500 text-left">deck: {props.card.deck}</div>
+      {templates[props.card.template].front(props.card as any)}
+      <hr class="h-px my-8 bg-slate-300 border-0"></hr>
+      {templates[props.card.template].back(props.card as any, props.showBack)}
+      {!props.showBack
+        ? <Button onClick={props.onReveal}>Reveal</Button>
+        : <Button onClick={props.onNext}>Next</Button>}
+    </div>
   );
 }
